0e2d0ad32e1df93c31c8e4201b74f33913051c40
klinck004 <keenan.linck@ontariotechu.net>
2023-12-03 21:42:07 -0500

    Minor bootstrap fixes
    Co-Authored-By: RyanChungOntariotechu <146674355+RyanChungOntariotechu@users.noreply.github.com>
    Co-Authored-By: meap9898 <145497717+meap9898@users.noreply.github.com>

--- server/config/app.js
+++ server/config/app.js
@@ -70,7 +70,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Access static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
